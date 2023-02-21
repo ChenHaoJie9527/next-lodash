a35babcd22b5226b23277bec872b777e95fa6d16
chenhaojie <a18520145493@163.com>
2023-02-21 20:20:34 +0800

    feat: 更新last类型编程运算

--- src/lib/array/last.ts
+++ src/lib/array/last.ts
@@ -1,8 +1,16 @@
-const last = <T extends unknown[]>(array: T): T[0] | undefined => {
-  if (!Array.isArray(array) || array.length === 0) {
-    return undefined;
-  }
-  return array[array.length - 1];
+type Last<T extends readonly unknown[]> = T extends readonly [
+  ...unknown[],
+  infer K
+]
+  ? K
+  : T[0] | undefined;
+
+const last = <T extends readonly any[]>(args: T): Last<T> => {
+  return args[args.length - 1];
 };
+
+const lastVal = last([1, 2] as const);
+const r1 = last([1, 2, 3]);
+const r2 = last([1, 2, "asd"]);
 
 export default last;
