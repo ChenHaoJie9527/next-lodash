475ca39a19d1c64c971eda17319f726735d01c27
chenhaojie9527 <a18520145493@163.com>
2023-02-28 00:15:22 +0800

    feat: chunk

--- src/lib/array/chunk.ts
+++ src/lib/array/chunk.ts
@@ -1,3 +1,28 @@
+/**
+ * Creates an array of elements split into groups the length of chunkSize.
+ *
+ * @since 1.0.0
+ *
+ * @template T
+ * @param {T[]} array - The array to process.
+ * @param {number} [chunkSize=1] - The length of each chunk.
+ * @param {T[][]} [cache=[]] - The array to store the chunked elements.
+ * @returns {T[][]} - The new array of chunks.
+ *
+ * @example
+ *
+ * chunk([1, 2, 3, 4, 5], 2);
+ * // returns [[1, 2], [3, 4], [5]]
+ *
+ * chunk([1, 2, 3, 4, 5], 3);
+ * // returns [[1, 2, 3], [4, 5]]
+ *
+ * chunk([1, 2, 3, 4, 5], 0);
+ * // returns []
+ * 
+ * chunk([1, 2, 3, 4, 5] as const,, 2)
+ * // returns [[1, 2], [3, 4], [5]]
+ */
 function chunk<T>(arr: readonly T[], size: number = 1, cache: T[][] = []): T[][] {
   const tmp = [...arr];
   const lent = arr === null ? 0 : arr.length;
@@ -10,6 +35,4 @@ function chunk<T>(arr: readonly T[], size: number = 1, cache: T[][] = []): T[][]
   }
   return cache;
 }
-const arr1 = chunk(["a", "b", "c", 1] as const, 2);
-console.log("arr1", arr1);
 export default chunk;
