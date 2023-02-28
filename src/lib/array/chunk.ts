1989f7cb0d3c41398b8b892e78514d7cfe421066
chenhaojie <a18520145493@163.com>
2023-02-28 19:35:28 +0800

    feat: 完善chunk类型

--- src/lib/array/chunk.ts
+++ src/lib/array/chunk.ts
@@ -23,16 +23,62 @@
  * chunk([1, 2, 3, 4, 5] as const,, 2)
  * // returns [[1, 2], [3, 4], [5]]
  */
-function chunk<T>(arr: readonly T[], size: number = 1, cache: T[][] = []): T[][] {
-  const tmp = [...arr];
-  const lent = arr === null ? 0 : arr.length;
-  if (!lent || size <= 0) {
+type Chunk<
+  Arr extends unknown[],
+  ItemLen extends number = 1,
+  CurItem extends unknown[] = [],
+  Res extends unknown[] = []
+> = ItemLen extends 0
+  ? Res
+  : Arr extends [infer First, ...infer Rest]
+  ? CurItem["length"] extends ItemLen
+    ? Chunk<Rest, ItemLen, [First], [...Res, CurItem]>
+    : Chunk<Rest, ItemLen, [...CurItem, First], Res>
+  : [...Res, CurItem];
+
+type Chunk1<T extends unknown[], L extends number = 1> = L extends 0
+  ? []
+  : [...T][];
+
+function chunk<T>(arr: T[]): Chunk1<T[], 1>;
+function chunk<T, K extends number>(arr: T[], size: K): Chunk1<T[], K>;
+function chunk<
+  T extends readonly unknown[],
+  I extends number,
+  R extends unknown[]
+>(arr: T, size: I, cache: R): Chunk<[...T], I, [], R>;
+function chunk<T extends readonly unknown[], I extends number>(
+  arr: T,
+  size: I
+): Chunk<[...T], I>;
+function chunk<T extends readonly unknown[]>(arr: T): Chunk<[...T]>;
+function chunk(
+  arr: readonly unknown[],
+  size: number = 1,
+  cache: unknown[] = []
+) {
+  const tmp = arr.concat([]);
+  const len = tmp === null ? 0 : tmp.length;
+  if (!len || size <= 0) {
     return cache;
   }
 
   while (tmp.length) {
     cache.push(tmp.slice(0, size));
   }
+
   return cache;
 }
+// const value = [1, 2, 3, 4, 5, 6, 7] as const;
+// const value1 = [1, 2, 3, 4, 5, "a"];
+// const res1 = chunk(value1);
+// const res2 = chunk(value1, 2);
+
+// type Result = Chunk<[1, 2, 3, 4, 5]>;
+
+// const a = chunk(value, 1);
+// const b = chunk(value, 2);
+// const c = chunk(value, 3);
+// const d = chunk(value);
+
 export default chunk;
