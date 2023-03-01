05edfd4dbb8a1f594e3d750cc245ab711688d317
chenhaojie9527 <a18520145493@163.com>
2023-03-02 00:40:14 +0800

    feat: concat2.0

--- src/test/concat.spec.ts
+++ src/test/concat.spec.ts
@@ -2,16 +2,18 @@ import { describe, it, expect } from "vitest";
 import { concat } from "../lib/array";
 
 describe("array/concat", () => {
-  it("should concat", () => {
-    const res = concat([1, 2, 3], [4, 5, 6]);
-    expect(res).toBeDefined();
-    expect(res.length).not.toBe(0);
-    expect(res.length).toBe(6);
+  it("should concat two array correctly", () => {
+    const arr1 = [1, 2, 3] as const;
+    const arr2 = [4, 5, 6] as const;
+    const res = concat(arr1, arr2);
+    expect(res).toEqual([1, 2, 3, 4, 5, 6]);
   });
-  it("concat params not array", () => {
-    const res = concat([1], 2, 3, 4, 5, false, "a", null, undefined);
-    expect(res).toBeDefined();
-    expect(res.length).not.toBe(0);
-    expect(res.length).toBe(9);
+
+  it("should concat a value to an array correctly", () => {
+    const arr1 = [1, 2, 3] as const;
+    const val = 4 as const;
+
+    const res = concat(arr1, val);
+    expect(res).toEqual([1, 2, 3, 4]);
   });
 });
