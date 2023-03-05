c4bc0057e42159609fd85fd3eb5c21c59d877de7
chenhaojie9527 <a18520145493@163.com>
2023-03-05 23:22:34 +0800

    feat: compact 类型函数 100%

--- src/lib/array/compact.ts
+++ src/lib/array/compact.ts
@@ -1,14 +1,33 @@
 type Falsey = null | undefined | false | "" | 0 | 0n;
-type Compact<T extends readonly unknown[], K = Falsey, C = []> = T extends [
-  infer F,
-  ...infer U
-]
-  ? F extends K
-    ? Compact<[...U], K, [F]>
-    : Compact<[...U], K, []>
-  : never;
-type Res = Compact<[1, 0, 1, false, 2, "", 3]>;
+type NotEqual<A, B> = (<T>() => T extends A ? 1 : 2) extends <
+  T
+>() => T extends B ? 1 : 2
+  ? false
+  : true;
 
-function compact() {}
+type Compact<
+  T extends readonly unknown[],
+  Res extends unknown[] = []
+> = T extends readonly [infer F, ...infer Reset]
+  ? Compact<
+      Reset,
+      F extends Falsey
+        ? Res
+        : NotEqual<F, number> extends false
+        ? Res
+        : [...Res, F]
+    >
+  : Res;
+
+type Res = Compact<["1", 1, 2, 3, 4, undefined, null, "", 0, typeof NaN]>; // ["1", 1, 2, 3, 4]
+const arr = [1, 2, undefined, null, 0, NaN, false, 3];
+
+function compact<T extends readonly unknown[]>(array: T): Compact<T> | [] {
+  if (!array.length || !Array.isArray(array) || array === null) {
+    return [];
+  }
+  const res = array.filter((item) => item);
+  return res as Compact<T>;
+}
 
 export default compact;
