024a10c2f76a71267c37767f781250276590b35b
chenhaojie <a18520145493@163.com>
2023-03-10 18:37:02 +0800

    feat: 新增chainable

---
+++ src/lib/object/chainable.ts
@@ -0,0 +1,30 @@
+type MyOmit<T, K> = {
+  [P in keyof T as P extends K ? never : P]: T[P];
+};
+
+type Config<T = {}> = {
+  option<K extends string, V extends any>(
+    key: K extends keyof T ? never : K,
+    value: V
+  ): Config<MyOmit<T, K> & { [P in K]: V }>;
+  get(): T;
+};
+
+function chainable<
+  K extends string,
+  U extends any,
+  T extends Record<string, any> = {}
+>(target: T, key: K, value: U) {
+  const config: Config<T> = {
+    option(key, value) {
+      return config;
+    },
+    get() {
+      return target;
+    },
+  };
+
+  return config;
+}
+
+export default chainable;
