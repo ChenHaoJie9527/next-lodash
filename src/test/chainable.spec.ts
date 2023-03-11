4f9ab960d42bea3f66dd5b84bb5c95d0ce9e35c2
chenhaojie9527 <a18520145493@163.com>
2023-03-12 00:15:55 +0800

    feat: 通过测试案例2

--- src/test/chainable.spec.ts
+++ src/test/chainable.spec.ts
@@ -1,10 +1,30 @@
 import { describe, it, expect } from "vitest";
 import { chainable } from "../lib";
 describe("object/chainable", () => {
-  it("获取config", () => {
+  it("调用chainable函数，第一个参数类型为{}", () => {
     const config = chainable({});
     expect(config).toBeDefined();
     expect(config).not.toBeUndefined();
     expect(config).not.toBeNull();
+    expect(config).toHaveProperty("option");
+    expect(config).toHaveProperty("get");
+    const target = config.get();
+    expect(target).toBeDefined();
+    expect(target).not.toBeUndefined();
+    expect(target).not.toBeNull();
+    expect(target).toEqual({});
+  });
+  it("调用chainable函数，第一个参数类型为{name: 'zzy', age: 18}", () => {
+    const config = chainable({ name: "zzy", age: 18 });
+    expect(config).toBeDefined();
+    expect(config).not.toBeUndefined();
+    expect(config).not.toBeNull();
+    expect(config).toHaveProperty("option");
+    expect(config).toHaveProperty("get");
+    const target = config.get();
+    expect(target).toBeDefined();
+    expect(target).not.toBeUndefined();
+    expect(target).not.toBeNull();
+    expect(target).toEqual({name: "zzy", age: 18});
   });
 });
