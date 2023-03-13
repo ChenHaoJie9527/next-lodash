eb7b51cdd6895014e60583821ea7d43f52ee5732
chenhaojie <a18520145493@163.com>
2023-03-13 17:55:13 +0800

    feat: 更新last

--- src/lib/array/last.ts
+++ src/lib/array/last.ts
@@ -5,12 +5,16 @@ type Last<T extends readonly unknown[]> = T extends readonly [
   ? K
   : T[0] | undefined;
 
-const last = <T extends readonly any[]>(args: T): Last<T> => {
-  return args[args.length - 1];
-};
+type Last1<T extends any[]> = T extends [...infer K, infer U] ? U : T[number];
+
+function last<T extends any[]>(args: T): Last1<T>;
+function last<T extends readonly any[]>(args: T): Last<T>;
+function last(args = []) {
+  return args.length ? args[args.length - 1] : undefined;
+}
 
 const lastVal = last([1, 2] as const);
-const r1 = last([1, 2, 3]);
-const r2 = last([1, 2, "asd"]);
+const r1 = last([1, 2, 3] as const);
+const r2 = last([1, 2, "asd"] as const);
 
 export default last;
